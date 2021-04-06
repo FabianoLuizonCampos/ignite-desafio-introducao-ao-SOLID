bdc990ec35f20a420d6dc0af3513d71d1613fbfa
FabianoLuizonCampos <fabianoluizoncampos@gmail.com>
2021-04-06 16:50:31 -0300

    chore: challenge concluded

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,22 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        // Complete aqui
+        try {
+            const { user_id } = request.headers;
+
+            const allUsers = this.listAllUsersUseCase.execute({
+                user_id: user_id.toString(),
+            });
+
+            return response.status(201).json(allUsers);
+        } catch (error) {
+            return response.status(400).json({ error });
+        }
+    }
 }
 
 export { ListAllUsersController };
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -2,15 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class TurnUserAdminUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User {
+        // Complete aqui
+        const userAlreadyExists = this.usersRepository.findById(user_id);
+
+        if (!userAlreadyExists) {
+            throw new Error("User already exists!");
+        }
+
+        const updatedUser = this.usersRepository.turnAdmin(userAlreadyExists);
+
+        return updatedUser;
+    }
 }
 
 export { TurnUserAdminUseCase };
